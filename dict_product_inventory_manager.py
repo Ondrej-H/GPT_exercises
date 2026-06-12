827949c755c4b8fe0578e5ad118aad01c846f70d
Ondřej Hubert <ondrej.hubert@gmail.com>
2026-06-12 11:40:01 +0200

    Add fn show_menu()

--- dict_product_inventory_manager.py
+++ dict_product_inventory_manager.py
@@ -1,4 +1,15 @@
 # PRODUCT INVENTORY MANAGER
 #  -----------------------
 
+def show_menu():
+    print("""
+1 - Add product
+2 - View product
+3 - Update quantity
+4 - Delete product
+5 - Show statistics
+6 - Show all products
+7 - Exit
+""")
+    
 
