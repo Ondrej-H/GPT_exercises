1606692d9bc5d36173bbb9cedb1d766f171d9fc9
Ondřej Hubert <ondrej.hubert@gmail.com>
2026-06-13 14:05:24 +0200

    Add fn view_product() to dict_product_inventory_manager.py

--- dict_product_inventory_manager.py
+++ dict_product_inventory_manager.py
@@ -1,6 +1,18 @@
 # PRODUCT INVENTORY MANAGER
 #  -----------------------
 
+# product inventory for testing
+inventory = {
+    "apple": {
+        "price": 25,
+        "quantity": 10
+    },
+    "banana": {
+        "price": 30,
+        "quantity": 5
+    }
+}
+
 def show_menu():
     print("""
 1 - Add product
@@ -34,3 +46,22 @@ def add_product(product_inventory: dict) -> None:
 """product_inventory = {}
 add_product(product_inventory)
 print(product_inventory)"""
+
+
+def view_product(product_inventory: dict) -> None:
+    product_to_view = input("Name of the product to view: ")
+
+    product_info = product_inventory.get(product_to_view)
+
+    if product_info is None:
+        print("Product not found.")
+        return
+    
+    print(f"Name of the product: {product_to_view}")
+    print(f"Price: {product_info['price']}")
+    print(f"Quantity: {product_info['quantity']}")
+
+
+# test view_product()
+"""view_product(inventory)"""
+
