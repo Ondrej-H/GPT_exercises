10acc03f676d3f3c35fc7e31e86a8a09e24ff1a2
Ondřej Hubert <ondrej.hubert@gmail.com>
2026-06-14 13:42:56 +0200

    Initialize dict_personal_library_manager.py

---
+++ dict_personal_library_manager.py
@@ -0,0 +1,19 @@
+# PERSONAL LIBRARY MAGER
+#  --------------------
+
+# library for testing
+library = {
+    "1984": {
+        "author": "George Orwell",
+        "year": 1949,
+        "read": True
+    },
+
+    "Dune": {
+        "author": "Frank Herbert",
+        "year": 1965,
+        "read": False
+    }
+}
+
+
